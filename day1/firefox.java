3b32d7e928f9d2370231ef7c615d5ef69459c27b
Vimala-1995 <139777758+Vimala-1995@users.noreply.github.com>
2023-07-18 15:54:31 +0530

    Add files via upload
    WEEK1 DAY 1 ASSIGNMENT

---
+++ day1/firefox.java
@@ -0,0 +1,16 @@
+package weekday1.day1;
+
+public class firefox {
+
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+double version=100.2;
+String name="firefox";
+Boolean visible= true;
+int year= 1998;
+char logo='m';
+System.out.println("version: " +version +"   "+"name: "+name+"");
+System.out.println("visible: " +visible +"   "+ "year: "+year+"");
+System.out.println("logo:" + logo+" ");
+}
+}
